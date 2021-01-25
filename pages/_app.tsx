92cbf380babdac578bf8e1db71962bef8837b1e5
GoG <forsoapfc@gmail.com>
2021-01-26 07:33:23 +0800

    Initial setup with typescript

---
+++ pages/_app.tsx
@@ -0,0 +1,20 @@
+import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />{" "}
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
